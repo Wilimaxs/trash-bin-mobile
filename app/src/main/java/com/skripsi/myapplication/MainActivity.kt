7cfb57f70db853df4fec2f8508d15dd1217f4ff6
wildantisna <wildan27370@gmail.com>
2026-06-21 10:26:09 +0700

    change theme for icon APP, Splash Screen and name APP

--- app/src/main/java/com/skripsi/myapplication/MainActivity.kt
+++ app/src/main/java/com/skripsi/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.skripsi.myapplication.core.theme.SmartTrashBinTheme
 import com.skripsi.myapplication.route.AppNavigation
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,6 +12,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
